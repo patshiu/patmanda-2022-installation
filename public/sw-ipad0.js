da3edde840bbacf6f7fa75024029749f48c73f73
pat shiu <shiu.patricia@gmail.com>
2022-01-03 22:58:07 -0500

    Adding caching for iPad0

---
+++ public/sw-ipad0.js
@@ -0,0 +1,40 @@
+const staticCacheName = 'iPad0Cache-v1';
+const assets = [
+    'ipad0.html',
+    'https://dl.dropboxusercontent.com/s/kd0gc5cv1ay3b4l/iPad0.mp4?dl=0',
+    'style/normalize.css',
+    'style/styles.css',
+    'https://cdn.socket.io/4.4.0/socket.io.min.js',
+    'vid-controls.js'   
+];
+
+// install service worker
+self.addEventListener('install', evt => { 
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+// activate event
+self.addEventListener('activate', evt => { 
+    evt.waitUntil(
+        caches.keys().then(keys => { 
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
+});
+
+// fetch event
+self.addEventListener('fetch', evt => { 
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => { 
+            return cacheRes || fetch(evt.request);
+        })
+    )
+})
